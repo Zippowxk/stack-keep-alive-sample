3479db1b3fde392868b579f6178716c6fc625bc0
王鑫凯 <wangxinkai@haier.com>
2022-05-02 20:16:58 +0800

    chore: fix issue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view v-slot="{ Component }">
+    <stack-keep-alive v-slot='{ key }'>
+      <component :is="Component"
+                 :key='key' />
+    </stack-keep-alive>
+  </router-view>
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import StackKeepAlive from 'stack-keep-alive'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(StackKeepAlive).mount('#app')
