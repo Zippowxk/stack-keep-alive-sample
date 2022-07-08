2e509c9a0c6d9ba0d7ab9b772b603c16d96b9aab
王鑫凯 <wangxinkai@haier.com>
2022-07-08 15:36:40 +0800

    chore: test vant replace router

---
+++ src/App3.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <router-view v-slot="{ Component }">
+      <sk-transition name='fade'>
+        <stack-keep-alive v-slot='{ key }' :replaceStay='["/", "/list"]'>
+          <component :is="Component"
+                     :key='key' />
+        </stack-keep-alive>
+      </sk-transition>
+    </router-view>
+    <van-tabbar route>
+      <van-tabbar-item replace
+                       to="/"
+                       icon="home-o">Home</van-tabbar-item>
+      <van-tabbar-item replace
+                       to="/list"
+                       icon="search">List</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+  },
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import { createApp } from 'vue'
-import App from './App2.vue'
-import router from './router'
-// import StackKeepAlive from 'stack-keep-alive'
-import StackKeepAlive from '../debug/index'
-
-createApp(App).use(router).use(StackKeepAlive).mount('#app')
+import { createApp } from "vue";
+import App from "./App3.vue";
+import router from "./router";
+import StackKeepAlive from 'stack-keep-alive'
+// import StackKeepAlive from "../debug/index";
+import { Tabbar, TabbarItem } from 'vant'
+createApp(App)
+  .use(router)
+  .use(StackKeepAlive)
+  .use(Tabbar)
+  .use(TabbarItem)
+  .mount("#app")
