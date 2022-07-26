176e01bd1bca67c09433aeb2cbe34baab9ea0f16
王鑫凯 <wangxinkai@haier.com>
2022-07-26 18:12:46 +0800

    chore: release

--- src/main.js
+++ src/main.js
@@ -6,12 +6,5 @@ import StackKeepAlive from 'stack-keep-alive'
 // import StackKeepAlive from '../debug/index'
 let app = createApp(App)
 
-// force open devtools 
-if ('__VUE_DEVTOOLS_GLOBAL_HOOK__' in window) {
-  if (window.__VUE_DEVTOOLS_GLOBAL_HOOK__) {
-    window.__VUE_DEVTOOLS_GLOBAL_HOOK__.emit("init", app);
-  }
-}
-app.config.devtools = true
 app.use(router).use(StackKeepAlive).mount('#app')
 
